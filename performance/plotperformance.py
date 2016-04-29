61ff98b0aaaa7a2bd7109fe2a097ac26cf9a73d0
Gabriel Moita <Gabriel Moita>
2016-04-29 00:39:56 -0300

    no message

--- performance/plotperformance.py
+++ performance/plotperformance.py
@@ -1,31 +1,31 @@
 import matplotlib.pyplot as plt
 
-with open("20160428180251.txt") as f:
+with open("20160428224143.txt") as f:
     data = f.read()
 
 data = data.split('\n')
 
-x = [i for i in range(len(data))]
+x = [i for i in range(0,len(data))]
 y = [row for row in data]
 
 fig = plt.figure()
 
 ax1 = fig.add_subplot(111)
 
-ax1.set_title("'Simulated Annealing'")
+ax1.set_title("Extended Local Beam Search")
 ax1.set_xlabel('Iterations')
 ax1.set_ylabel('Performance')
-#ax1.axvline(10,0,20000)
-#ax1.axvline(110,0,20000)
-#ax1.axvline(210,0,20000)
-#ax1.axvline(310,0,20000)
-#ax1.axvline(410,0,20000)
-#ax1.axvline(510,0,20000)
-#ax1.axvline(610,0,20000)
+
+ax1.axvline(9,0,20000)
+for i in range(109,4809,100):
+    ax1.axvline(i,0,20000)
 
 ax1.plot(x,y, c='r', marker='o', ls='')
 
 leg = ax1.legend()
 
+plt.xlim(0, 4809)
+plt.ylim(0, 20000)
+
 plt.show()
-fig.savefig('SimulatedAnnealing.png')
+fig.savefig('ExtendedLocalBeamSearch.png')
